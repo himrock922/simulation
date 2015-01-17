c64838120d3a907ecc53a04c81553537f88be984
八木 辰弥 <himrock922@yagi-no-MacBook-Pro.local>
2015-01-17 19:42:19 +0900

    update

---
+++ cluster_storm.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+	ruby cluster.rb 1000node.txt > result2
+	ruby cluster.rb 2000node.txt >> result2
+	ruby cluster.rb 3000node.txt >> result2
+	ruby cluster.rb 4000node.txt >> result2
+	ruby cluster.rb 5000node.txt >> result2
+	ruby cluster.rb 6000node.txt >> result2
+	ruby cluster.rb 7000node.txt >> result2
+	ruby cluster.rb 8000node.txt >> result2
+	ruby cluster.rb 9000node.txt >> result2
+	ruby cluster.rb 10000node.txt >> result2
+	ruby cluster.rb 20000node.txt >> result2
+	ruby cluster.rb 30000node.txt >> result2
+	ruby cluster.rb 40000node.txt >> result2
+	ruby cluster.rb 50000node.txt >> result2
+	ruby cluster.rb 60000node.txt >> result2
+	ruby cluster.rb 70000node.txt >> result2
+	ruby cluster.rb 80000node.txt >> result2
+	ruby cluster.rb 90000node.txt >> result2
+	ruby cluster.rb 100000node.txt >> result2
+
